82cc5edae8f4c6fd279f5b9b5b70a199aaa2f0d1
n1727131 <n1727131@noreply.localhost>
2023-10-23 13:42:50 +0200

    Dateien nach "/" hochladen
    -first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediTrack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void API_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            // Handle Button 1 click event
+        }
+
+        private void SELECT_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            // Handle Button 2 click event
+        }
+
+        private void ADD_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void POWER_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
