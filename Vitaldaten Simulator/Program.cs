dd1aa4867206b67cc57595e80542f878dfaafd67
TheAllMaker <selcukd44@hotmail.de>
2023-10-25 00:52:01 +0200

    simulator erster Test

---
+++ Vitaldaten Simulator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitaldaten_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Random r = new Random();
+            int Herzschlag = r.Next(60, 80);
+            Console.WriteLine("Herzschlag Test:" + Herzschlag);
+            Console.ReadLine();
+
+
+        }
+    }
+}
