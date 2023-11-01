a1c9c80a5a66a4dbc798dbbfd769f3a40ccd7c3d
1921233 <83212565+1921233@users.noreply.github.com>
2023-11-01 22:47:53 +0100

    add addPatient SQL Query

--- Database/Program.cs
+++ Database/Program.cs
@@ -9,22 +9,40 @@ namespace Database
 {
     class Program
     {
+        private static String connString = "Host=db.inftech.hs-mannheim.de;Username=n1921233;Password=123456;Database=n1921233_meditrack";
+
+       
+
         static void Main(string[] args)
         {
 
-            var connString = "Host=db.inftech.hs-mannheim.de;Username=n1921233;Password=123456;Database=n1921233_meditrack";
-
+            
 
             var conn = new NpgsqlConnection(connString);
-            conn.Open();
-
-
-            // Verbindung schließen
-            conn.Close();
+            
+            Patient p = new Patient(prename: "Hans", surname: "Müller",birthday: new DateTime(1994, 06, 14));
+            addPatient(p);
 
 
         }
-        static void addPatient(MediTrack.Patient p) { p.Patient = p; }
+        static  void addPatient( Patient p) {
+            var conn = new NpgsqlConnection(connString);
+            conn.Open();
+            var sql = "INSERT INTO patients(prename, surname, birthday) VALUES(@prename,@surname,@birthday)";
+            using (var cmd = new NpgsqlCommand(sql, conn))
+            {
+                cmd.Parameters.AddWithValue("prename", p.prename);
+                cmd.Parameters.AddWithValue("surname", p.surname);
+                cmd.Parameters.AddWithValue("birthday", p.birthday);
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+            }
+            Console.WriteLine("row inserted");
+
+
+            conn.Close();
+            ;
+        }
 
     }
 }
