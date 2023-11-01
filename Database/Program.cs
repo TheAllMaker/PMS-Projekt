703049421c6a33782d2d89305780f5f5d65a09c4
1921233 <83212565+1921233@users.noreply.github.com>
2023-11-01 22:00:04 +0100

    add new Project for Database

--- Database/Program.cs
+++ Database/Program.cs
@@ -3,28 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Npgsql;
 
 namespace Database
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-
-            var connString = "Host=db.inftech.hs-mannheim.de;Username=n1921233;Password=123456;Database=n1921233_meditrack";
-
-
-            var conn = new NpgsqlConnection(connString);
-            conn.Open();
-
-
-            // Verbindung schließen
-            conn.Close();
-
-
         }
-        static void addPatient(MediTrack.Patient p) { p.Patient = p; }
-
     }
 }
