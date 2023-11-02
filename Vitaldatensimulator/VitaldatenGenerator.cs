fac35ca5816da59f265d5bc0fcfa314b9859f9da
TheAllMaker <selcukd44@hotmail.de>
2023-11-02 11:43:20 +0100

    Logik in generierung der Vitaldaten eingebaut

--- Vitaldatensimulator/VitaldatenGenerator.cs
+++ Vitaldatensimulator/VitaldatenGenerator.cs
@@ -9,31 +9,60 @@ namespace Vitaldatensimulator
     internal class VitaldatenGenerator
     {
         private static Random r = new Random();
+        private int aktuellerHerzschlag = 80; // Startwert
+        private int aktuelleAtemfrequenz = 16; // Startwert
+        private int aktuelleSauerstoffsättigung = 98; // Startwert
+        private int aktuellerSystolischerBlutdruck = 120; // Startwert
+        private int aktuellerDiastolischerBlutdruck = 80; // Startwert
+
+        //public int GeneriereHerzschlag()
+        //{
+          //  return r.Next(60, 100);
+        //}
 
         public int GeneriereHerzschlag()
         {
-            return r.Next(60, 100);
+            return GeneriereRealistischenWert(ref aktuellerHerzschlag, 60, 100, -2, 2);
         }
 
         public int GeneriereAtemfrequenz()
         {
-            return r.Next(12, 20);
+            return GeneriereRealistischenWert(ref aktuelleAtemfrequenz, 12, 20, -2, 2);
         }
 
         public int GeneriereSauerstoffsättigung()
         {
-            return r.Next(95, 100);
+            return GeneriereRealistischenWert(ref aktuelleSauerstoffsättigung, 95, 100, -1, 1);
         }
 
         public int GeneriereSystolischerBlutdruck()
         {
-            return r.Next(110, 130);
+            return GeneriereRealistischenWert(ref aktuellerSystolischerBlutdruck, 110, 130, -3, 3);
         }
 
         public int GeneriereDiastolischerBlutdruck()
         {
-            return r.Next(70, 90);
+            return GeneriereRealistischenWert(ref aktuellerDiastolischerBlutdruck, 70, 90, -3, 3);
         }
+
+        public int GeneriereRealistischenWert(ref int aktuellerWert, int minWert, int maxWert, int minÄnderung, int maxÄnderung)
+        {
+            int aenderung = r.Next(minÄnderung, maxÄnderung + 1);
+            aktuellerWert += aenderung;
+
+            // Überprüfen, ob der neue Wert im Bereich liegt
+            if (aktuellerWert < minWert)
+            {
+                aktuellerWert = minWert;
+            }
+            else if (aktuellerWert > maxWert)
+            {
+                aktuellerWert = maxWert;
+            }
+
+            return aktuellerWert;
+        }
+
 
         public void GeneriereVitaldaten(PatientVitalDaten patient)
         {
