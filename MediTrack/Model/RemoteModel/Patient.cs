22989c2e7b7c72af6cf14a2c3f60ffc4fb6cc45b
n1727131 <1727131@stud.hs-mannheim.de>
2023-11-03 22:35:35 +0100

    Dynamic TextView Addition

---
+++ MediTrack/Model/RemoteModel/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediTrack.Model.RemoteModel
+{
+    public class Patient
+    {
+        public string? PatientNameI { get; set; }
+        public string? PatientNameII { get; set; }
+        public int? PatientNumber { get; set; }
+        public float? PatientMonitor { get;set; }
+        public float PatientRespirationRate { get; set; }
+        public float PatientTemperature { get; set; }
+        public float PatientHeartRate { get; set; }
+        public float PatientOxygen { get; set; }
+        public float PatientPressure { get; set;}
+        public float PatientScore { get; set;}
+    }
+    
+}
