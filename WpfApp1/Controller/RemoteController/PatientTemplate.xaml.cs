a468c96d56aa86b49564d8058444b12076ce47ae
Selcuk Demirtas <1925665@stud.hs-mannheim.de>
2023-11-27 19:36:39 +0100

    small build fix wegen namensänderung

--- WpfApp1/Controller/RemoteController/PatientTemplate.xaml.cs
+++ WpfApp1/Controller/RemoteController/PatientTemplate.xaml.cs
@@ -13,14 +13,14 @@ namespace MediTrack.Controller.RemoteController
         
         public void OpenDetailed(object sender, RoutedEventArgs e)
         {
-            Window DetailedWindow = new Vitaldatensimulator.MainWindow
-            {
-                Title = "DetailWindow",
-                Width = SystemParameters.PrimaryScreenWidth * 0.75,
-                Height = SystemParameters.PrimaryScreenHeight * 0.75,
-                WindowStartupLocation = WindowStartupLocation.CenterScreen
-            };
-            DetailedWindow.Show();
+//            Window DetailedWindow = new Vitaldatensimulator.MainWindow
+//           {
+//                Title = "DetailWindow",
+//                Width = SystemParameters.PrimaryScreenWidth * 0.75,
+//                Height = SystemParameters.PrimaryScreenHeight * 0.75,
+//                WindowStartupLocation = WindowStartupLocation.CenterScreen
+//            };
+//            DetailedWindow.Show();
             //DetailedWindow.Owner = this;
         }
     }
