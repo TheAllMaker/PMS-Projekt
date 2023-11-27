f361ff78117366cda31a6a45e9dacb9393af3960
Selcuk Demirtas <1925665@stud.hs-mannheim.de>
2023-11-27 18:42:47 +0100

    Ausversehen Dateien nicht gepusht sorry

---
+++ Vitaldatensimulator/MainCreatePatientWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vitaldatensimulator
+{
+    public partial class MainCreatePatientWindow : Window
+    {
+        public MainCreatePatientWindow()
+        {
+            InitializeComponent();
+        }
+        // Klick-Handler für "Zoom In" Button
+        private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Zoom In" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Respiration Rate Decrease" Button
+        private void RespirationRateDecrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Respiration Rate Decrease" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Respiration Rate Increase" Button
+        private void RespirationRateIncrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Respiration Rate Increase" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Oxygen Saturations Decrease" Button
+        private void OxygenSaturationsDecrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Oxygen Saturations Decrease" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Oxygen Saturations Increase" Button
+        private void OxygenSaturationsIncrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Oxygen Saturations Increase" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Temperature Decrease" Button
+        private void TemperatureDecrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Temperature Decrease" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Temperature Increase" Button
+        private void TemperatureIncrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Temperature Increase" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Systolic BP Decrease" Button
+        private void SystolicBPDecrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Systolic BP Decrease" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Systolic BP Increase" Button
+        private void SystolicBPIncrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Systolic BP Increase" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Heart Rate Decrease" Button
+        private void HeartRateDecrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Heart Rate Decrease" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Heart Rate Increase" Button
+        private void HeartRateIncrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Heart Rate Increase" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Consciousness Decrease" Button
+        private void ConsciousnessDecrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Consciousness Decrease" Button geklickt wird.
+        }
+
+        // Klick-Handler für "Consciousness Increase" Button
+        private void ConsciousnessIncrease_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code einfügen, der ausgeführt werden soll, wenn der "Consciousness Increase" Button geklickt wird.
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
