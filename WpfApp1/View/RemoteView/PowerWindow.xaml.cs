16e973b99c825d8ce3238a87fe6accb0b54dd455
TheAllMaker <selcukd44@hotmail.de>
2023-11-29 22:12:02 +0100

    Powerwindow nun komplett gefixxt und es schließt nun alles

--- WpfApp1/View/RemoteView/PowerWindow.xaml.cs
+++ WpfApp1/View/RemoteView/PowerWindow.xaml.cs
@@ -7,6 +7,8 @@ namespace MediTrack.View.RemoteView
 {
     public partial class PowerWindow : Window
     {
+        private bool UserConfirmed { get; set; }
+
         public PowerWindow()
         {
             InitializeComponent();
@@ -15,18 +17,34 @@ namespace MediTrack.View.RemoteView
 
         private void PowerWindowClosing(object sender, CancelEventArgs e)
         {
-            var confirmationDialog = new ConfirmationDialog();
-            confirmationDialog.Owner = this;
+            ShowConfirmationDialog();
 
-            confirmationDialog.ShowDialog();
-
-            if (!confirmationDialog.UserConfirmed)
+            if (!UserConfirmed)
             {
                 e.Cancel = true;
             }
         }
 
+        private void ShowConfirmationDialog()
+        {
+            // Weisen Sie die Ereignishandler zu
+            ConfirmButton.Click += Button_Click_Confirm;
+            CancelButton.Click += Button_Click_Cancel;
+        }
+
         private void Button_Click_Confirm(object sender, RoutedEventArgs e)
+        {
+            UserConfirmed = true;
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_Cancel(object sender, RoutedEventArgs e)
+        {
+            UserConfirmed = false;
+            Close();
+        }
+
+        private void Button_Click_Confirm_PowerWindow(object sender, RoutedEventArgs e)
         {
             // Implementierung für den Confirm-Button
             var button = sender as Button; // Wenn nötig
@@ -34,28 +52,11 @@ namespace MediTrack.View.RemoteView
             this.Close(); // Schließt das Fenster
         }
 
-        private void Button_Click_Cancel(object sender, RoutedEventArgs e)
+        private void Button_Click_Cancel_PowerWindow(object sender, RoutedEventArgs e)
         {
             // Implementierung für den Cancel-Button
             var button = sender as Button; // Wenn nötig
             // ... Weitere Aktionen beim Abbrechen
-        }
-    }
-
-    public partial class ConfirmationDialog : Window
-    {
-        public bool UserConfirmed { get; private set; }
-
-        private void Button_Click_Confirm(object sender, RoutedEventArgs e)
-        {
-            UserConfirmed = true;
-            Close();
-        }
-
-        private void Button_Click_Cancel(object sender, RoutedEventArgs e)
-        {
-            UserConfirmed = false;
-            Close();
         }
     }
 }
