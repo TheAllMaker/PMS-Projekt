c8b1ad11278fd1c23dcd7da16e51c02173e5f01b
n1727131 <1727131@stud.hs-mannheim.de>
2023-12-02 16:42:32 +0100

    HashSet für MonitorIDs hinzugefügt + Kommentar Erklärung

---
+++ WpfApp1/Model/RemoteModel/HashSet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediTrack.Model.RemoteModel
+{
+    public static class HashSet
+    {
+     private static HashSet<int> MonitorHashSet = new HashSet<int>();
+
+        public static bool InsertMonitorKeyToHash(int MonitorKey)
+        {
+         return MonitorHashSet.Add(MonitorKey);
+        }
+
+        public static bool RemoveMonitorKeyToHash(int MonitorKey)
+        {
+          return MonitorHashSet.Remove(MonitorKey);
+        }
+
+        public static bool ContainsHashMonitorKey (int MonitorKey)
+        {
+           return MonitorHashSet.Contains(MonitorKey);
+        }
+        // -> für Selcuk: Wie greife ich darauf zu ? -> in deinem File: 1) namespace Zugriff deklarieren + 2) Assembly reinhauen 
+        // + 3) dann zugreifen mit classennamen.klassenemthode -> also hier HashSet.InsertMonitorKeyToHash(MonitorID);
+        // Wie würde ich es machen:
+        // if( HashSet.ContainsHashMonitorKey (int MonitorKey) == false)
+        // { if(HashSet.InsertMonitorKeyToHash == true) Console.Writeline("Insert succesful blablalbab else dann ne Nachricht
+        // NICHT VERGESSEN: die Zugriffe geben Bools zurück so dass du sehen kannst was schief gelaufen ist und dich daran orientieren kannst !
+    }
+}
