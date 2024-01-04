dfc0414dfbc67f8ce99fd815debfab3fb51c3b50
Selcuk Demirtas <selcukd44@hotmail.de>
2024-01-04 19:16:26 +0100

    aussehen erneut geändert

--- Vitaldatensimulator/PowerWindow.xaml.cs
+++ Vitaldatensimulator/PowerWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace MediTrack.View.RemoteView
         {
             if (!isAlreadyClosing)
             {
-                this.Close();
+                Close();
             }
         }
     }
