b02310bfef84328a0410bd8b53afd94868825ffd
n1727131 <1727131@stud.hs-mannheim.de>
2024-01-06 22:03:16 +0100

    Bug Search

--- WpfApp1/View/RemoteView/CrossButton.xaml.cs
+++ WpfApp1/View/RemoteView/CrossButton.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -25,10 +26,91 @@ namespace MediTrack.View.RemoteView
         }
 
 
-        private void ShowOptions_Click1(object sender, RoutedEventArgs e)
+
+
+        private void ShowBlockOptions(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("soyoz Neruwinlju resiuvnik");
+            ////var popup = Application.Current.FindRe("CrossButtonOptionsPopUp") as Popup;
+            //var popup = e.FindName("CrossButtonOptionsPopUp") as Popup;
+            //if (popup != null)
+            //{
+            //    popup.IsOpen = true;
+            //}
+            var control = sender as FrameworkElement;
+            if (control != null)
+            {
+                // Suchen des Popups im gleichen Namensraum wie 'sender'
+                var popup = control.FindName("CrossButtonOptionsPopUp") as Popup;
+                if (popup != null)
+                {
+                    popup.IsOpen = true;
+                }
+            }
         }
+
+
+
+
+        //ContentControl newContent = new ContentControl();
+        //newContent.ContentTemplate = this.Resources["PatientTemplate"] as DataTemplate;
+        //        newContent.Content = Application.Current.Resources["TestPatient2"]; // Set the content you want to display
+
+        //     PatientNe9tworkIcon.Content = newContent;
+
+        //        // Close the popup if necessary
+        //        OptionsPopup.IsOpen = false;
+        //        // Zurücksetzen der Auswahl im Popup und Schließen des Popups
+        //        OptionsListBox.SelectedItem = null;
+        //        OptionsPopup.IsOpen = false;
+        //    }
+
+        private void CrossButtonSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            var control = sender as FrameworkElement;
+            if (control != null)
+            {
+
+
+
+                var itemCount = control.FindName("CrossButtonListBox") as ListBox;
+                Console.WriteLine("ddwdw");
+                if (itemCount.SelectedItem != null)
+                {
+
+                    ContentControl newContentControlForGrid = new ContentControl();
+                    newContentControlForGrid.ContentTemplate =control.FindResource("CrossButton") as DataTemplate;
+                    //var dataTemplate = this.FindResource("CrossButton") as DataTemplate;
+
+
+
+
+                    // Hinzufügen des neuen ContentControls zum nächsten UniformGrid
+                    //MainWindow.PatientenMonitorDynGrid.Children.Add(newContentControlForGrid);
+
+                    // Aktualisieren des ContentTemplates des ausgewählten CrossButtons
+                    //if (PatientNe9tworkIcon.Content is ContentControl currentContentControl)
+                    //{
+                    //    currentContentControl.ContentTemplate = (DataTemplate)FindResource("PatientTemplate");
+                    //    //currentContentControl.Content = Application.Current.Resources["TestPatient2"] /* Hier das entsprechende Content-Objekt setzen */;
+                    //    //    ContentTemplate = (DataTemplate)Resources["PatientTemplate"],
+                    //    //    Content = Application.Current.Resources["TestPatient2"],
+                    //}
+                    ContentControl newContent = new ContentControl();
+                    newContent.ContentTemplate = control.FindResource("PatientTemplate") as DataTemplate;
+                    newContent.Content = Application.Current.Resources["TestPatient2"]; // Set the content you want to display
+                    var CrossButtonBlock = control.FindName("CrossButtonBlock") as ToggleButton;
+                    CrossButtonBlock.Content = newContent;
+                    var popup = control.FindName("CrossButtonOptionsPopUp") as Popup;
+                    // Close the popup if necessary
+                    //popup.IsOpen = false;
+                    // Zurücksetzen der Auswahl im Popup und Schließen des Popups
+                    itemCount.SelectedItem = null;
+                    popup.IsOpen = false;
+                }
+            }
+        }
+
 
         //private void OptionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         //{
