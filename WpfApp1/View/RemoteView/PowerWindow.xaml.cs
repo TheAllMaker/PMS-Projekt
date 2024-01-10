0e5647cccf10ce3ac9a1ab648fb8ee355c9f3c3a
n1727131 <1727131@stud.hs-mannheim.de>
2024-01-10 01:19:20 +0100

    Working Power Button

--- WpfApp1/View/RemoteView/PowerWindow.xaml.cs
+++ WpfApp1/View/RemoteView/PowerWindow.xaml.cs
@@ -2,34 +2,18 @@
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using Vitaldatensimulator;
 
 namespace MediTrack.View.RemoteView
 {
     public partial class PowerWindow : Window
     {
-        private bool UserConfirmed { get; set; }
+        public event EventHandler ConfirmClicked;
+        private bool UserConfirmed = false;
 
         public PowerWindow()
         {
             InitializeComponent();
-            Closing += PowerWindowClosing;
-        }
-
-        private void PowerWindowClosing(object sender, CancelEventArgs e)
-        {
-            ShowConfirmationDialog();
-
-            if (!UserConfirmed)
-            {
-                e.Cancel = true;
-            }
-        }
-
-        private void ShowConfirmationDialog()
-        {
-            // Weisen Sie die Ereignishandler zu
-            ConfirmButton.Click += Button_Click_Confirm;
-            CancelButton.Click += Button_Click_Cancel;
         }
 
         private void Button_Click_Confirm(object sender, RoutedEventArgs e)
@@ -43,22 +27,5 @@ namespace MediTrack.View.RemoteView
             UserConfirmed = false;
             Close();
         }
-
-        private void Button_Click_Confirm_PowerWindow(object sender, RoutedEventArgs e)
-        {
-            // Implementierung für den Confirm-Button
-            var button = sender as Button; // Wenn nötig
-            // ... Weitere Aktionen beim Bestätigen
-            this.Close(); // Schließt das Fenster
-        }
-
-        private void Button_Click_Cancel_PowerWindow(object sender, RoutedEventArgs e)
-        {
-            // Implementierung für den Cancel-Button
-            var button = sender as Button; // Wenn nötig
-            // ... Weitere Aktionen beim Abbrechen
-        }
-
-        
     }
 }
