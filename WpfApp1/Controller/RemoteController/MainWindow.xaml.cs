5674d7a0e42d10661ff274068faa1f0ae593362c
Selcuk Demirtas <1925665@stud.hs-mannheim.de>
2024-01-11 04:31:33 +0100

    fix?

--- WpfApp1/Controller/RemoteController/MainWindow.xaml.cs
+++ WpfApp1/Controller/RemoteController/MainWindow.xaml.cs
@@ -1,28 +1,14 @@
-﻿using MediTrack.Controller.RemoteController;
-using MediTrack.Model.DataBaseModelConnection;
+﻿using MediTrack.Model.DataBaseModelConnection;
 using MediTrack.Model.RemoteModel;
-using MediTrack.Properties;
 using MediTrack.View.RemoteView;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.Metrics;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace MediTrack
 {
