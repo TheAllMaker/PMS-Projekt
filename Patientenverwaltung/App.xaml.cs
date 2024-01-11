9b828e5adfd15b156bf3822f56db394fe7afe354
n1721587 <1721587@stud.hs-mannheim.de>
2024-01-11 01:42:47 +0100

    App.xaml startpunkt gesetzt

--- Patientenverwaltung/App.xaml.cs
+++ Patientenverwaltung/App.xaml.cs
@@ -13,5 +13,11 @@ namespace Patientenverwaltung
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //base.OnStartup(e);
+            //MainMenu mainMenu = new MainMenu();
+            //mainMenu.Show();
+        }
     }
 }
